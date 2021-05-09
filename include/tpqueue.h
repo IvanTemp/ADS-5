fca3e2aa953bfa3437bf503d8bfa7c753e66fbff
IvanTemp <78906461+IvanTemp@users.noreply.github.com>
2021-05-09 16:39:32 +0300

    elkofjkofjg

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -5,37 +5,37 @@
 
 struct SYM {
 public:
-	SYM* pNext;
-	char data;
-	int prior;
+SYM* pNext;
+char data;
+int prior;
 
-	SYM(char data, int prior, SYM* pNext = nullptr) {
-		this->data = data;
-		this->prior = prior;
-		this->pNext = pNext;
-	}
+SYM(char data, int prior, SYM* pNext = nullptr) {
+this->data = data;
+this->prior = prior;
+this->pNext = pNext;
+}
 };
 
 template<typename T>
 class TPQueue {
 public:
-	TPQueue();
-	~TPQueue();
+TPQueue();
+~TPQueue();
 
-	char pop_front();
-	void push_back(SYM item);
-	void push_front(SYM item);
-	void push(SYM item);
-	int GetSize() { return Size; };
+char pop_front();
+void push_back(SYM item);
+void push_front(SYM item);
+void push(SYM item);
+int GetSize() { return Size; };
 private:
-	int Size;
-	SYM *head;
+int Size;
+SYM *head;
 };
 
 template<typename T>
 TPQueue<T>::TPQueue() {
-	Size = 0;
-	head = nullptr;
+Size = 0;
+head = nullptr;
 }
 
 template<typename T>
@@ -44,61 +44,61 @@ TPQueue<T>::~TPQueue() {
 
 template<typename T>
 char TPQueue<T>::pop_front() {
-	SYM *temp = head;
-	head = head->pNext;
-	char data = temp->data;
-	delete temp;
-	Size--;
-	return data;
+SYM *temp = head;
+head = head->pNext;
+char data = temp->data;
+delete temp;
+Size--;
+return data;
 }
 
 template<typename T>
 void TPQueue<T>::push_back(SYM item) {
-	if (head == nullptr) {
-		head = new SYM(item);
-	} else {
-		SYM* current = this->head;
-		while (current->pNext != nullptr) {
-			current = current->pNext;
-		}
-		current->pNext = new SYM(item);
-	}
-	Size++;
+if (head == nullptr) {
+head = new SYM(item);
+} else {
+SYM* current = this->head;
+while (current->pNext != nullptr) {
+current = current->pNext;
+}
+current->pNext = new SYM(item);
+}
+Size++;
 }
 
 template<typename T>
 void TPQueue<T>::push_front(SYM item) {
-	head = new SYM(item);
-	Size++;
+head = new SYM(item);
+Size++;
 }
 
 template<typename T>
 void TPQueue<T>::push(SYM item) {
-	if (head == nullptr) {
-		//push front
-		head = new SYM(item.data, item.prior, head);
-		Size++;
-	} else {
-		SYM* current = this->head;
-		//Если входящий элемент имеет высокий приоритет
-		if (current->prior < item.prior) {
-			//push front
-			head = new SYM(item.data, item.prior, head);
-			Size++;
-			return;
-		}
-		//Проход по списку в поисках того самого элемента
-		while (current->pNext != nullptr) {
-			if (current->pNext->prior > item.prior) {
-				current = current->pNext;
-			} else {
-				break;
-			}
-		}
-		SYM* newNode = new SYM(item.data, item.prior, current->pNext);
-		current->pNext = newNode;
-		Size++;
-	}
+if (head == nullptr) {
+//push front
+head = new SYM(item.data, item.prior, head);
+Size++;
+} else {
+SYM* current = this->head;
+//Если входящий элемент имеет высокий приоритет
+if (current->prior < item.prior) {
+//push front
+head = new SYM(item.data, item.prior, head);
+Size++;
+return;
+}
+//Проход по списку в поисках того самого элемента
+while (current->pNext != nullptr) {
+if (current->pNext->prior > item.prior) {
+current = current->pNext;
+} else {
+break;
+}
+}
+SYM* newNode = new SYM(item.data, item.prior, current->pNext);
+current->pNext = newNode;
+Size++;
+}
 }
 
 
